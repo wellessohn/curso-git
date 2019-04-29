0d9697afb3e24ebeafa59bdb54087b2e0d2a9957
Bruno.Silva <wellessohn@gmail.com>
2019-04-29 20:31:44 -0300

    mae doente

--- Git/Program.cs
+++ Git/Program.cs
@@ -9,7 +9,7 @@ namespace Git
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("aeae");
-            Console.WriteLine("como vc esta hoj");
+            Console.WriteLine("cminha mae ta doente");
         }
     }
 }
